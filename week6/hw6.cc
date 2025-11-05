f98fbc505af5c43bc6f89e0afe2b6c6d479805d9
kmbae1142 <kmbae1142@gmail.com>
2025-11-05 22:34:39 +0900

    modify

--- week6/hw6.cc
+++ week6/hw6.cc
@@ -7,10 +7,10 @@ int main() {
     TreeUtil* tree_util = TreeUtil::GetInstance();
 
     Node* node7 = new IntNode(7, nullptr, nullptr);
-    Node* node6 = new IntNode(6, nullptr, nullptr);
+    Node* node6 = new StringNode("FF", nullptr, nullptr);
     Node* node5 = new IntNode(5, node6, node7);
     Node* node4 = new IntNode(4, nullptr, nullptr);
-    Node* node3 = new IntNode(3, nullptr, nullptr);
+    Node* node3 = new StringNode("CC", nullptr, nullptr);
     Node* node2 = new IntNode(2, node4, node5);
     Node* node1 = new IntNode(1, node2, node3);
     
@@ -18,15 +18,20 @@ int main() {
     std::cout << tree_util->InOrderTraversal(node1) << '\n';
     std::cout << tree_util->PostOrderTraversal(node1) << '\n';
     
-    Node* nodeG = new StringNode("G", nullptr, nullptr);
-    Node* nodeF = new StringNode("F", nullptr, nullptr);
-    Node* nodeE = new StringNode("E", nodeF, nodeG);
-    Node* nodeD = new StringNode("D", nullptr, nullptr);
-    Node* nodeC = new StringNode("C", nullptr, nullptr);
-    Node* nodeB = new StringNode("B", nodeD, nodeE);
-    Node* nodeA = new StringNode("A", nodeB, nodeC);
+    Node* nodeG = new StringNode("GG", nullptr, nullptr);
+    Node* nodeF = new StringNode("FF", nullptr, nullptr);
+    Node* nodeE = new StringNode("EE", nodeF, nodeG);
+    Node* nodeD = new StringNode("DD", nullptr, nullptr);
+    Node* nodeC = new StringNode("CC", nullptr, nullptr);
+    Node* nodeB = new StringNode("BB", nodeD, nodeE);
+    Node* nodeA = new StringNode("AA", nodeB, nodeC);
     
     std::cout << tree_util->PreOrderTraversal(nodeA) << '\n';
     std::cout << tree_util->InOrderTraversal(nodeA) << '\n';
     std::cout << tree_util->PostOrderTraversal(nodeA) << '\n';
+
+    Node* node_1 = new IntNode(1, nullptr, nullptr);
+    std::cout << tree_util->PreOrderTraversal(node_1) << '\n';
+    std::cout << tree_util->InOrderTraversal(node_1) << '\n';
+    std::cout << tree_util->PostOrderTraversal(node_1) << '\n';
 }
